9666fe17cbf524800df9cf4f2453e910351f2a9b
Matt Crinklaw-Vogt <mlaw@fb.com>
2020-05-21 22:45:20 -0400

    add and remove children

--- prototype/main.js
+++ prototype/main.js
@@ -2,53 +2,62 @@ const d3Dag = require('d3-dag');
 const d3Base = require('d3');
 const d3 = {...d3Dag, ...d3Base};
 
-const data = [
-	{
-		id: 'Blog',
-	}, {
-		id: 'Philosophy',
-		parentIds: ['Blog'],
-	}, {
-		id: 'Religion',
-		parentIds: ['Blog'],
-	}, {
-		id: 'Theology',
-		parentIds: ['Religion', 'Philosophy'],
-	}, {
-		id: 'Language',
-		parentIds: ['Blog'],
-	}, {
-		id: 'Types',
-		parentIds: ['Language', 'SoftwareDevelopment'],
-	}, {
-		id: 'SoftwareDevelopment',
-		parentIds: ['Blog'],
-	}, {
-		id: 'Programming',
-		parentIds: ['SoftwareDevelopment', 'Language'],
-	}
+const allData = [
+  {
+    id: 'Tantamanlands',
+  }, {
+    id: 'Philosophy',
+    parentIds: ['Tantamanlands'],
+  }, {
+    id: 'Religion',
+    parentIds: ['Tantamanlands'],
+  }, {
+    id: 'Theology',
+    parentIds: ['Religion', 'Philosophy'],
+  }, {
+    id: 'Language',
+    parentIds: ['Tantamanlands'],
+  }, {
+    id: 'Types',
+    parentIds: ['Language', 'SoftwareDevelopment'],
+  }, {
+    id: 'SoftwareDevelopment',
+    parentIds: ['Tantamanlands'],
+  }, {
+    id: 'Programming',
+    parentIds: ['SoftwareDevelopment', 'Language'],
+  }
 ];
+
+function addChildren(data, rootId) {
+  const t = data.concat(allData.filter(n => (n.parentIds || []).indexOf(rootId) !== -1));
+  console.log(t);
+  return t;
+}
+
+function removeChildren(data, rootId) {
+  return data.filter(n => n.parentIds.indexOf(rootId) === -1);
+}
 
 function htmlToElement(html) {
   const template = document.createElement('template');
   html = html.trim(); // Never return a text node of whitespace as the result
-	template.innerHTML = html;
+  template.innerHTML = html;
   return template.content.firstChild;
 }
 
-function makeNav() {
-	const textFill = '#CCCCCC';
-	const textHighlightFill = 'orange';
-	const reader = d3.dagStratify();
-	const dag = reader(data);
+function makeNav(data) {
+  const textFill = '#CCCCCC';
+  const textHighlightFill = 'orange';
+  const reader = d3.dagStratify();
 
-	const layout = d3.sugiyama()
-		.size([window.innerWidth - 60, window.innerHeight - 60])
-		.layering(d3.layeringLongestPath())
-		.decross(d3.decrossOpt())
-		.coord(d3.coordCenter());
+  const layout = d3.sugiyama()
+    .size([window.innerWidth - 60, window.innerHeight - 60])
+    .layering(d3.layeringLongestPath())
+    .decross(d3.decrossOpt())
+    .coord(d3.coordCenter());
 
-	// This code only handles rendering
+  // This code only handles rendering
   const nodeRadius = 20;
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -69,44 +78,46 @@ function makeNav() {
     .append("path")
     .attr("d", "M 0 0 12 6 0 12 3 6")
     .style("fill", "#666666");
-    
   
-  // Use computed layout
-  layout(dag);
-  
-  const steps = dag.size();
-  const interp = d3.interpolateRgb('#666666', '#AAAAAA');
-  const colorMap = {};
-  dag.each((node, i) => {
-    colorMap[node.id] = interp(i / steps);
-  });
-  
-  // How to draw edges
   const line = d3.line()
     .curve(d3.curveCatmullRom)
     .x(d => d.x)
     .y(d => d.y);
+  const interp = d3.interpolateRgb('#666666', '#AAAAAA');
+
+  function constructDag(data) {
+    const dag = reader(data);
+    layout(dag);
+    
+    const steps = dag.size();
+    const colorMap = {};
+    dag.each((node, i) => {
+      colorMap[node.id] = interp(i / steps);
+    });
+
+    return dag;
+  }
 
   function plotEdges(dag) {
     svgSelection.append('g')
-	    .selectAll('path')
-	    .data(dag.links())
-	    .enter()
-	    .append('path')
-	    .attr('d', ({ data }) => {
-	    	const [start, end] = data.points;
-	    	// Should technically shift the X as well so we retain
-	    	// the original slop of the line.
-	    	return line([start, {x: end.x, y: end.y - 10}]);
-	    })
-	    .attr('fill', 'none')
-	    .attr('stroke-width', 2)
-	    .attr('stroke', '#666666')
-	    .attr('marker-end', 'url(#arrow)');
+      .selectAll('path')
+      .data(dag.links())
+      .enter()
+      .append('path')
+      .attr('d', ({ data }) => {
+        const [start, end] = data.points;
+        // Should technically shift the X as well so we retain
+        // the original slop of the line.
+        return line([start, {x: end.x, y: end.y - 10}]);
+      })
+      .attr('fill', 'none')
+      .attr('stroke-width', 2)
+      .attr('stroke', '#666666')
+      .attr('marker-end', 'url(#arrow)');
   }
 
   function plotNodes() {
- 	  const nodes = svgSelection.append('g')
+    const nodes = svgSelection.append('g')
       .selectAll('g')
       .data(dag.descendants())
       .enter()
@@ -115,35 +126,39 @@ function makeNav() {
 
     nodes.append('text')
       .text(d => d.id)
-	    .attr('class', 'node-label')
-	    .attr('font-weight', 'bold')
-	    .attr('font-family', 'sans-serif')
-	    .attr('text-anchor', 'middle')
-	    .attr('alignment-baseline', 'middle')
-	    .attr('fill', textFill)
-	    .attr('fill-opacity', 1)
-	    .attr('stroke', '#000000')
-	    .attr('stroke-width', 0.5)
-	    .attr('stroke-opacity', 1)
-	    .on('mouseover', onTextMouseOver)
-	    .on('mouseout', onTextMouseOut)
-	    .on('click', onTextClick);
+      .attr('class', 'node-label')
+      .attr('font-weight', 'bold')
+      .attr('font-family', 'sans-serif')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .attr('fill', textFill)
+      .attr('fill-opacity', 1)
+      .attr('stroke', '#000000')
+      .attr('stroke-width', 0.5)
+      .attr('stroke-opacity', 1)
+      .on('mouseover', onTextMouseOver)
+      .on('mouseout', onTextMouseOut)
+      .on('click', onTextClick);
   }
 
+
+  const dag = constructDag(data);
   plotEdges(dag);
   plotNodes(dag);
 
   function onTextMouseOver() {
-  	d3.select(this).attr('fill', textHighlightFill);
+    d3.select(this).attr('fill', textHighlightFill);
   }
 
   function onTextMouseOut() {
-  	d3.select(this).attr('fill', textFill);	
+    d3.select(this).attr('fill', textFill); 
   }
 
   function onTextClick() {
-  	console.log('add some data');
+    const dag = constructDag(addChildren(data, d3.select(this).data()[0].id));
+    plotEdges(dag);
+    plotNodes(dag);
   }
 }
 
-makeNav();
+makeNav([{id: 'Tantamanlands'}]);
